eda683505acaf190e599b65d7496f9c4c0ba1ff9
Bunnbuns <ben@bunnbuns.net>
2019-04-05 08:28:02 -0400

    fixed removing of theme breaking all posts without the theme

--- scripts/post.js
+++ scripts/post.js
@@ -180,7 +180,10 @@ window.onload = function () {
 	if (stuff.poster == "herronjo" || stuff.poster == "DomHupp" || stuff.poster == "Aldeenyo" || stuff.poster == "savaka" || stuff.poster == "-Verso-" || stuff.poster == "Bunnbuns") {
 		document.getElementById("content").innerHTML = stuff.content.replace(/\r\n/g, "<br/>");
         // fix for reddit theme inside of reddit theme:
-        console.log("remove reddit theme"); document.getElementById("post-reddit-theme").disabled = true;
+        var postRedditThemeLink = document.getElementById("post-reddit-theme");
+        if(postRedditThemeLink){
+            console.log("remove reddit theme"); postRedditThemeLink.disabled = true;
+        }
 	} else {
 		document.getElementById("content").innerHTML = stuff.content.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\r\n/g, "<br/>");
 	}
